cd36b3c5ab974d0f01ab0429d419e7897ea55796
eekulesa <emilieeileen@icloud.com>
2021-04-17 18:57:40 +0100

    rnadom pick function

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+function getRandomPosition(element) {
+//   const grid = document.querySelector('.grid')
+// 	var x = grid.offsetHeight-element.clientHeight;
+// 	var y = grid.offsetWidth-element.clientWidth;
+// 	var randomX = Math.floor(Math.random()*x);
+// 	var randomY = Math.floor(Math.random()*y);
+// 	return [randomX,randomY];
+// }
+// window.onload = function() {
+//   const grid = document.querySelector('.grid')
+// 	var img = document.createElement('img');
+// 	img.setAttribute("style", "position: relative;");
+// 	img.setAttribute("src", "./images/recycle.png");
+//   grid.appendChild(img);
+// 	var xy = getRandomPosition(img);
+//   console.log(xy)
+// 	img.style.top = xy[0] + 'px';
+// 	img.style.left = xy[1] + 'px';
+// }
