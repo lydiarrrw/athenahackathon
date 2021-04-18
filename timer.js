4ca2dee4d708968f45d011450b5f9bec1a7fe6a7
Lydia <lydiaawood@googlemail.com>
2021-04-18 10:58:05 +0100

    update

--- timer.js
+++ timer.js
@@ -1,27 +1,17 @@
-const finaleDate = new Date("March 19, 2022 00:00:00").getTime();
+var timeLeft = 30
+var elem = document.getElementById('timer')
+var timerId = setInterval(countdown, 1000)
 
-const timer = () => {
-  const now = new Date().getTime();
-  let diff = finaleDate - now
-  // Showing the alert when the counter time finishes.
-  if (diff < 0) {
-    document.querySelector('.alert').style.display = 'block'
-    document.querySelector('.container').style.display = 'none'
+function countdown() {
+  if (timeLeft === -1) {
+    clearTimeout(timerId)
+    doSomething()
+  } else {
+    elem.innerHTML = timeLeft
+    timeLeft--
   }
-
-  let days = Math.floor(diff / (1000 * 60 * 60 * 24))
-  let hours = Math.floor(diff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
-  let minutes = Math.floor(diff % (1000 * 60 * 60) / (1000 * 60))
-  let seconds = Math.floor(diff % (1000 * 60) / 1000)
-  // Adding the zeros
-  minutes <= 9 ? minutes = `0${minutes}` : minutes
-  seconds <= 9 ? seconds = `0${seconds}` : seconds
-
-
-  document.querySelector('#minutes').textContent = minutes
-  document.querySelector('#seconds').textContent = seconds
-
 }
-timer()
-// Calling the function every 1000 milliseconds.
-setInterval(timer, 1000)
+
+function doSomething() {
+  alert('Time\'s Up!')
+}
